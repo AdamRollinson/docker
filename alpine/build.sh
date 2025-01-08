79e23efeb5ff3174b0bcfcc5e1d25e0427081a9d
Adam Rollinson <adamleerollinson@gmail.com>
2025-01-08 22:14:20 +0000

    Update to build shell and added client max size to app.conf.template

--- alpine/build.sh
+++ alpine/build.sh
@@ -9,21 +9,9 @@ PHP_VERSIONS=("8.1" "8.2" "8.3" "8.4")
 PHP_IMAGE="adamrollogi/php"
 PHP_NGINX_IMAGE="adamrollogi/php-nginx"
 
-# Check for --push flag
-PUSH_IMAGES=false
-if [[ "$1" == "--push" ]]; then
-    PUSH_IMAGES=true
-    echo "🔄 Push flag detected. Images will be pushed to the registry after building."
-fi
-
 # Build base image
 echo "🚀 Building base image: $BASE_IMAGE"
 docker build -t "$BASE_IMAGE" ./base
-
-if [ "$PUSH_IMAGES" = true ]; then
-    echo "📤 Pushing base image: $BASE_IMAGE"
-    docker push "$BASE_IMAGE"
-fi
 
 # Build PHP images
 for version in "${PHP_VERSIONS[@]}"; do
@@ -35,11 +23,6 @@ for version in "${PHP_VERSIONS[@]}"; do
         --build-arg PHP_VERSION=$PHP_VERSION_ENV \
         -t "$IMAGE_TAG" \
         ./php
-
-    if [ "$PUSH_IMAGES" = true ]; then
-        echo "📤 Pushing PHP image: $IMAGE_TAG"
-        docker push "$IMAGE_TAG"
-    fi
 done
 
 # Build PHP-NGINX images
@@ -53,15 +36,6 @@ for version in "${PHP_VERSIONS[@]}"; do
         --build-arg PHP_VERSION=$PHP_VERSION_ENV \
         -t "$IMAGE_TAG" \
         ./php-nginx
-
-    if [ "$PUSH_IMAGES" = true ]; then
-        echo "📤 Pushing PHP-NGINX image: $IMAGE_TAG"
-        docker push "$IMAGE_TAG"
-    fi
 done
 
 echo "✅ All images built successfully!"
-
-if [ "$PUSH_IMAGES" = true ]; then
-    echo "🚀 All images pushed successfully!"
-fi
